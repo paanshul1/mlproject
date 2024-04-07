3acaa110182d42a8310d4a20462bd3fefb287f79
Paanshul Chowdhary <paanshul.chowdhary1994@gmail.com>
2024-04-07 00:09:55 -0400

    data transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,12 +6,14 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 
 @dataclass # helps in defining class varable directly
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('aritfact',"train.csv")
-    test_data_path:str=os.path.join('aritfact',"test.csv")
-    raw_data_path:str=os.path.join('aritfact',"data.csv")
+    train_data_path:str=os.path.join('artifact',"train.csv")
+    test_data_path:str=os.path.join('artifact',"test.csv")
+    raw_data_path:str=os.path.join('artifact',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -42,6 +44,9 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-#if __name__=="__main__":
-#    obj=DataIngestion()
-#    obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
